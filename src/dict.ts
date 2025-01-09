de0f8c20da197b627d3c1bd214040b4e96f7c6e2
neverland <chenjiahan.jait@bytedance.com>
2025-01-09 19:17:17 +0800

    feat: add dict

---
+++ src/dict.ts
@@ -0,0 +1,23 @@
+import abbreviates from 'case-police/dict/abbreviates.json' with {
+  type: 'json',
+};
+import brands from 'case-police/dict/brands.json' with { type: 'json' };
+import general from 'case-police/dict/general.json' with { type: 'json' };
+import products from 'case-police/dict/products.json' with { type: 'json' };
+import softwares from 'case-police/dict/softwares.json' with { type: 'json' };
+
+export const dict = [
+  ...Object.values(abbreviates),
+  ...Object.values(brands),
+  ...Object.values(general),
+  ...Object.values(products),
+  ...Object.values(softwares),
+  'EJS',
+  'Rspack',
+  'Rsbuild',
+  'Rsdoctor',
+  'Rslib',
+  'Rspress',
+  'Rstest',
+  'Vite',
+];
--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,7 @@ import fs from 'node:fs';
 import color from 'picocolors';
 import { logger } from 'rslog';
 import { glob } from 'tinyglobby';
+import { dict } from './dict.js';
 
 export async function globMarkdownFiles(cwd: string) {
   return glob(['**/*.md', '**/*.mdx'], {
@@ -13,6 +14,8 @@ export async function globMarkdownFiles(cwd: string) {
 
 const isTitleLine = (line: string) => /^#{1,6}\s+\S/.test(line.trim());
 const isEnglishWord = (word: string) => /^[a-zA-Z]+$/.test(word);
+const isCamelCase = (word: string) => /^[a-z][a-zA-Z]*$/.test(word);
+const isTerm = (word: string) => dict.includes(word);
 
 const formatLine = (originalLine: string) => {
   let line = originalLine;
@@ -20,33 +23,41 @@ const formatLine = (originalLine: string) => {
 
   for (let index = 0; index < words.length; index++) {
     const word = words[index];
-    if (index === 0 || !isEnglishWord(word)) {
+
+    // Ignore:
+    // 1. the first word
+    // 2. non-English words
+    // 3. camelCase words
+    // 4. terms
+    if (
+      index <= 1 ||
+      !isEnglishWord(word) ||
+      isCamelCase(word) ||
+      isTerm(word)
+    ) {
       continue;
     }
 
     const lowerCaseWord = word.toLowerCase();
     if (lowerCaseWord !== word) {
-      line = line.replace(
-        new RegExp(`(\\s)${word}(\\s)`, 'g'),
-        `$1${lowerCaseWord}$2`,
-      );
+      line = line.replace(new RegExp(`\\b${word}\\b`, 'g'), lowerCaseWord);
     }
   }
 
-  return words.join(' ');
+  return line;
 };
 
 function formatContent(content: string) {
   const lines = content.split('\n');
 
-  lines.map((originalLine) => {
-    if (isTitleLine(originalLine)) {
-      return formatLine(originalLine);
-    }
-    return originalLine;
-  });
-
-  return lines.join('\n');
+  return lines
+    .map((originalLine) => {
+      if (isTitleLine(originalLine)) {
+        return formatLine(originalLine);
+      }
+      return originalLine;
+    })
+    .join('\n');
 }
 
 async function formatFile(filePath: string) {
